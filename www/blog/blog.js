f27746a2bb5a1bcb82748f18e74ef1d4c396db2f
Yizhe Liu <yizow@berkeley.edu>
2020-05-23 23:58:34 +0000

    add recipes section

--- www/blog/blog.js
+++ www/blog/blog.js
@@ -11,15 +11,14 @@ var template = 'template.html';
 // YYYY_MM_DD_BLOG_TITLE.md
 var blog_format = /([0-9]{4}_[0-9]{2}_[0-9]{2}_(.*)\.md)$/;
 
+// Blog proper
 http.createServer(function (request, response) {
-
   var blog_post = '0000_00_00_index.md'
   var query = url.parse(request.url, true).pathname;
 
   var is_indexed = /blog_posts\[([0-9]*)\]/;
   if (is_indexed.test(query)) {
     var index = parseInt(query.match(is_indexed)[1]);
-    console.log('index', index, query);
     // get blog_post by index
     blog_posts = blog_posts_by_name();
     if (index < blog_posts.length) {
@@ -48,6 +47,32 @@ http.createServer(function (request, response) {
     }
   });
 }).listen(8083);
+
+
+// Recipes
+var recipe_root = 'www/';
+
+http.createServer(function (request, response) {
+  var query = url.parse(request.url, true).pathname;
+  query = recipe_root + query;
+
+  if (!fs.existsSync(query)) {
+    response.writeHead(404, {'Content-Type': 'text/html'}).end('404\nNot found');
+    return;
+  }
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  var markdown = md.render(fs.readFileSync(query, 'utf-8'));
+
+  ejs.renderFile(blog_root + template, {title: query.match(/\/([^\/]*)\.md$/)[1], markdown: markdown}, {}, (err, str) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.end(str);
+    }
+  });
+}).listen(8084);
+
 
 function blog_posts_by_name() {
   return fs.readdirSync(blog_root, {withFileTypes: true})
